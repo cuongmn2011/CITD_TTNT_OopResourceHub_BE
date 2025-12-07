1eb378236bd058b3ef4818a9f49558dfa7a21cf3
10tinsinh <cuongmn2011@gmail.com>
2025-12-07 10:28:24 +0700

    fix: simplify Vercel config for native ASGI support

--- api/index.py
+++ api/index.py
@@ -1,12 +1,1 @@
-from mangum import Mangum
-
-
-def get_fastapi_app():
-	from app.main import app as _app
-	return _app
-
-
-app = get_fastapi_app()
-handler = Mangum(app, lifespan="off")
-
-__all__ = ("app", "handler")
+from app.main import app
